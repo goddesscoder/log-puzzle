338a9ed074b54693008189907bef72fbee1b9234
Bethsheba Zebata <bethzebata@gmail.com>
2020-10-20 19:44:56 -0500

    created a new branch for pull request

--- logpuzzle.py
+++ logpuzzle.py
@@ -13,7 +13,7 @@ Here's what a puzzle URL looks like (spread out onto multiple lines):
 HTTP/1.0" 302 528 "-" "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US;
 rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
 """
-__author__ = "Bethsheba Zebata, Study Hall, John"
+__author__ = "Bethsheba Zebata,Study Hall,John"
 
 import os
 import re
